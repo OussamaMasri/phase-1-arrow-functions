2b529c49480ce04f3fdf82cfce2e5c28db644ca2
Oussama Masri <oussamamasri16120@gmail.com>
2022-08-10 15:47:32 +0300

    Assignment Done

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+
+divide(2000,100)
+
+const square = (parameter1) => parameter1 ** 2;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
